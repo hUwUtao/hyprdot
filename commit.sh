602671dd73a6d81e5f69e6894f415b0a84e6094a
hUwUtao <root@hUwUtao.me>
2023-05-15 13:40:09 +0700

    upload files 15/3 (too lazy to count refs)

--- commit.sh
+++ commit.sh
@@ -1,2 +1,4 @@
 [[ ! -d "upload/.config" ]] && mkdir upload/.config -p
-cp -rv hypr btop cava starship.toml waybar wallpaper fish kitty rofi upload/.config
+cp -rv hypr btop cava starship.toml waybar wallpaper fish kitty rofi dunst upload/.config
+mkdir -p upload/.config/nvim/lua/custom/themes
+cp -rv nvim/lua/custom/themes/* upload/.config/nvim/lua/custom/themes
